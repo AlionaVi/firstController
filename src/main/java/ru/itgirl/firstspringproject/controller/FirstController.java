d7142ef517d0d856c07a2f415036013bb463c4e4
AlionaVi <alionakolendo@gmail.com>
2023-03-31 14:14:48 +0300

    Создан FirstController

---
+++ src/main/java/ru/itgirl/firstspringproject/controller/FirstController.java
@@ -0,0 +1,55 @@
+package ru.itgirl.firstspringproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    enum Day {
+
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY;
+    }
+
+    Day days;
+
+    @GetMapping("/dayOfWeek")
+    public String dayOfWeek(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        try {
+            days = Day.valueOf(name);
+        } catch (IllegalArgumentException e) {
+            return String.format("Hello %s!", name);
+        }
+
+        switch (days) {
+            case MONDAY:
+                return "Сегодня Понедельник!";
+            case TUESDAY:
+                return "Сегодня Вторник!";
+            case WEDNESDAY:
+                return "Сегодня Среда!";
+            case THURSDAY:
+                return "Сегодня Четверг!";
+            case FRIDAY:
+                return "Сегодня Пятница!";
+            case SATURDAY:
+                return "Сегодня Суббота!";
+            case SUNDAY:
+                return "Сегодня Воскресенье!";
+        }
+        return name;
+    }
+}
+
+
+
+
+
